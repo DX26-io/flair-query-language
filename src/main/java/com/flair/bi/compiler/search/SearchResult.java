0c416ec2dcd80ec1c3591250e364489d4b4832bf
Flair Viz Dev <flairvizentricdev17@gmail.com>
2021-06-08 22:52:54 +0300

    fix search result

--- src/main/java/com/flair/bi/compiler/search/SearchResult.java
+++ src/main/java/com/flair/bi/compiler/search/SearchResult.java
@@ -1,53 +1,53 @@
 package com.flair.bi.compiler.search;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 public class SearchResult {
 
-    private final List<IStatementResult> results = new ArrayList<>();
+    private AggregationStatementsResult aggregationStatementsResult;
+    private ByStatementResult byStatementResult;
+    private WhereStatementResult whereStatementResult;
+    private OrderByStatementResult orderByStatementResult;
 
-    public List<IStatementResult> getResults() {
-        return results;
+    public void setAggregationStatementsResult(AggregationStatementsResult aggregationStatementsResult) {
+        this.aggregationStatementsResult = aggregationStatementsResult;
     }
 
-    public void addResult(IStatementResult result) {
-        results.add(result);
+    public void setByStatementResult(ByStatementResult byStatementResult) {
+        this.byStatementResult = byStatementResult;
+    }
+
+    public void setWhereStatementResult(WhereStatementResult whereStatementResult) {
+        this.whereStatementResult = whereStatementResult;
+    }
+
+    public void setOrderByStatementResult(OrderByStatementResult orderByStatementResult) {
+        this.orderByStatementResult = orderByStatementResult;
     }
 
     public Optional<AggregationStatementsResult> asAggregationStatementsResult() {
-        if (results.size() >= 1) {
-            return Optional.ofNullable((AggregationStatementsResult) results.get(0));
-        }
-        return Optional.empty();
+        return Optional.ofNullable(aggregationStatementsResult);
     }
 
     public Optional<ByStatementResult> asByStatementResult() {
-        if (results.size() >= 2) {
-            return Optional.ofNullable((ByStatementResult) results.get(1));
-        }
-        return Optional.empty();
+        return Optional.ofNullable(byStatementResult);
     }
 
     public Optional<WhereStatementResult> asWhereStatementResult() {
-        if (results.size() >= 3) {
-            return Optional.ofNullable((WhereStatementResult) results.get(2));
-        }
-        return Optional.empty();
+        return Optional.ofNullable(whereStatementResult);
     }
 
     public Optional<OrderByStatementResult> asOrderByStatementResult() {
-        if (results.size() >= 4) {
-            return Optional.ofNullable((OrderByStatementResult) results.get(3));
-        }
-        return Optional.empty();
+        return Optional.ofNullable(orderByStatementResult);
     }
 
     @Override
     public String toString() {
-        return "{" +
-                "results=" + results +
+        return "SearchResult{" +
+                "aggregationStatementsResult=" + aggregationStatementsResult +
+                ", byStatementResult=" + byStatementResult +
+                ", whereStatementResult=" + whereStatementResult +
+                ", orderByStatementResult=" + orderByStatementResult +
                 '}';
     }
 }
